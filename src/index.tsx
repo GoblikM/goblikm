897a041ba73ec60339a371d10a1ea866f18132f9
Martin Goldbach <m_goldbach@utb.cz>
2024-06-05 11:41:36 +0200

    fixing routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, HashRouter } from "react-router-dom";
+import React from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
